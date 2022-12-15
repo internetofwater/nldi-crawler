ed41e583aebfdf461625148145be6abca610a159
g.trantham <gtrantham@contractor.usgs.gov>
2022-12-15 17:20:19 -0600

    essential cli options

--- src/nldi_crawler/cli.py
+++ src/nldi_crawler/cli.py
@@ -3,13 +3,20 @@
 # pylint: disable=fixme
 #
 #
+"""
+Command Line Interface for launching the NLDI web crawler.
+"""
+
 import click
 
 from . import __version__
 
+
 @click.command()
+@click.option("--list", "list_", is_flag=True, help="Show list of crawler sources and exit.")
 @click.version_option(version=__version__)
-def main():
+def main(list_):
     """CLI to launch NLDI crawler."""
     click.echo("Hello World.")
-    
+    if (list_):
+        click.echo("Listing sources.")
