540e921a8e484da73dfc27c5b49e7359c3286566
g.trantham <gtrantham@contractor.usgs.gov>
2022-12-16 09:20:59 -0600

    cmd line option for --config

--- src/nldi_crawler/cli.py
+++ src/nldi_crawler/cli.py
@@ -14,9 +14,12 @@ from . import __version__
 
 @click.command()
 @click.option("--list", "list_", is_flag=True, help="Show list of crawler sources and exit.")
+@click.option("--config","conf_", type=click.Path(exists=True), help="location of config file.")
 @click.version_option(version=__version__)
-def main(list_):
+def main(list_, conf_):
     """CLI to launch NLDI crawler."""
     click.echo("Hello World.")
     if (list_):
         click.echo("Listing sources.")
+    if (conf_):
+        click.echo(f"Using config file: {conf_}")
